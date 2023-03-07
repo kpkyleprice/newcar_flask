ec52290cf01ce1da6061b5d4b731371c896513af
Kyle Price <kpkyleprice@gmail.com>
2023-03-07 15:08:46 -0800

    updated config

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,6 +4,10 @@ from models import db, User, Contact, contact_schema, contacts_schema
 
 api = Blueprint('api',__name__, url_prefix='/api')
 
+@api.route('/getdata')
+def getdata():
+    return {'yee': 'haw'}
+
 @api.route('/contacts', methods = ['POST'])
 @token_required
 def create_contact(current_user_token):
